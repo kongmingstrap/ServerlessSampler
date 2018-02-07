a371b54014146eb178003c36c050a1159a279f10
Tanaka Takaaki <kongmings.trap@gmail.com>
2018-02-07 22:36:17 +0900

    Improve s3 mock tests

--- src/handlers/contribute_attendances/tests/conftest.py
+++ src/handlers/contribute_attendances/tests/conftest.py
@@ -3,6 +3,8 @@ import os
 import boto3
 import pytest
 
+from pathlib import Path
+
 
 def pytest_runtest_setup():
     """
@@ -22,16 +24,25 @@ def s3():
 
 
 @pytest.fixture(scope='function')
-def create_attendance_bucket(s3, request):
-    """
-    create s3 bucket
-    """
-
+def create_bucket(s3, request):
     bucket_name = os.environ['ATTENDANCE_DATA_BUCKET_NAME']
     s3.create_bucket(Bucket=bucket_name)
 
-    def delete_attendance_bucket():
-        bucket_name = os.environ['ATTENDANCE_DATA_BUCKET_NAME']
+    def delete_bucket():
         bucket = s3.Bucket(bucket_name)
         bucket.delete()
-    request.addfinalizer(delete_attendance_bucket)
+    request.addfinalizer(delete_bucket)
+
+
+@pytest.fixture(scope='function')
+def put_bucket_items(s3, request):
+    bucket_name = os.environ['ATTENDANCE_DATA_BUCKET_NAME']
+
+    for i in request.param:
+        key = i[0]
+        file = i[1]
+
+        rel_path = 'fixtures/{0}'.format(file)
+        filename = str(Path(__file__).parent.joinpath(rel_path).resolve())
+
+        s3.Object(bucket_name, key).upload_file(filename)
--- src/handlers/summary_users/tests/conftest.py
+++ src/handlers/summary_users/tests/conftest.py
@@ -82,16 +82,25 @@ def put_user_items(dynamodb):
 
 
 @pytest.fixture(scope='function')
-def create_attendance_bucket(s3, request):
-    """
-    create s3 bucket
-    """
-
+def create_bucket(s3, request):
     bucket_name = os.environ['ATTENDANCE_DATA_BUCKET_NAME']
     s3.create_bucket(Bucket=bucket_name)
 
-    def delete_attendance_bucket():
-        bucket_name = os.environ['ATTENDANCE_DATA_BUCKET_NAME']
+    def delete_bucket():
         bucket = s3.Bucket(bucket_name)
         bucket.delete()
-    request.addfinalizer(delete_attendance_bucket)
+    request.addfinalizer(delete_bucket)
+
+
+@pytest.fixture(scope='function')
+def put_bucket_items(s3, request):
+    bucket_name = os.environ['ATTENDANCE_DATA_BUCKET_NAME']
+
+    for i in request.param:
+        key = i[0]
+        file = i[1]
+
+        rel_path = 'fixtures/{0}'.format(file)
+        filename = str(Path(__file__).parent.joinpath(rel_path).resolve())
+
+        s3.Object(bucket_name, key).upload_file(filename)
