f5821b4289ca26754c6af56e657b7f34e8180102
Tanaka Takaaki <kongmings.trap@gmail.com>
2018-02-08 22:55:41 +0900

    Fix s3 fixtures

--- src/handlers/contribute_attendances/tests/conftest.py
+++ src/handlers/contribute_attendances/tests/conftest.py
@@ -1,9 +1,8 @@
 import os
+from pathlib import Path
 
 import boto3
 import pytest
-
-from pathlib import Path
 
 
 def pytest_runtest_setup():
@@ -17,32 +16,50 @@ def pytest_runtest_setup():
 @pytest.fixture(scope='session')
 def s3():
     """
-    s3 fixture
+    s3 resource fixture
     """
 
     return boto3.resource('s3', endpoint_url='http://localhost:4572')
 
 
+@pytest.fixture(scope='session')
+def s3_client():
+    """
+    s3 client fixture
+    """
+
+    return boto3.client('s3', endpoint_url='http://localhost:4572')
+
+
 @pytest.fixture(scope='function')
 def create_bucket(s3, request):
+    """
+    create bucket fixture
+    """
+
     bucket_name = os.environ['ATTENDANCE_DATA_BUCKET_NAME']
     s3.create_bucket(Bucket=bucket_name)
 
     def delete_bucket():
         bucket = s3.Bucket(bucket_name)
+        bucket.objects.all().delete()
         bucket.delete()
     request.addfinalizer(delete_bucket)
 
 
 @pytest.fixture(scope='function')
 def put_bucket_items(s3, request):
+    """
+    put object in bucket fixture
+    """
+
     bucket_name = os.environ['ATTENDANCE_DATA_BUCKET_NAME']
 
     for i in request.param:
         key = i[0]
         file = i[1]
 
-        rel_path = 'fixtures/{0}'.format(file)
+        rel_path = f'fixtures/{file}'
         filename = str(Path(__file__).parent.joinpath(rel_path).resolve())
 
         s3.Object(bucket_name, key).upload_file(filename)
--- src/handlers/summary_users/tests/conftest.py
+++ src/handlers/summary_users/tests/conftest.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 import boto3
 import pytest
@@ -29,6 +30,15 @@ def s3():
     """
 
     return boto3.resource('s3', endpoint_url='http://localhost:4572')
+
+
+@pytest.fixture(scope='session')
+def s3_client():
+    """
+    s3 client fixture
+    """
+
+    return boto3.client('s3', endpoint_url='http://localhost:4572')
 
 
 @pytest.fixture(scope='function')
@@ -83,24 +93,33 @@ def put_user_items(dynamodb):
 
 @pytest.fixture(scope='function')
 def create_bucket(s3, request):
+    """
+    create bucket fixture
+    """
+
     bucket_name = os.environ['ATTENDANCE_DATA_BUCKET_NAME']
     s3.create_bucket(Bucket=bucket_name)
 
     def delete_bucket():
         bucket = s3.Bucket(bucket_name)
+        bucket.objects.all().delete()
         bucket.delete()
     request.addfinalizer(delete_bucket)
 
 
 @pytest.fixture(scope='function')
 def put_bucket_items(s3, request):
+    """
+    put object in bucket fixture
+    """
+
     bucket_name = os.environ['ATTENDANCE_DATA_BUCKET_NAME']
 
     for i in request.param:
         key = i[0]
         file = i[1]
 
-        rel_path = 'fixtures/{0}'.format(file)
+        rel_path = f'fixtures/{file}'
         filename = str(Path(__file__).parent.joinpath(rel_path).resolve())
 
         s3.Object(bucket_name, key).upload_file(filename)
